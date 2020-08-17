bc79fd9a4b7dd4cc98cda23f189b4cd9e0e95528
gusguma <daganzo10>
2020-08-17 11:16:12 +0200

    Update appication.properties #1

--- src/main/java/es/gusguma/template/microservice/eureka/server/SystemResource.java
+++ src/main/java/es/gusguma/template/microservice/eureka/server/SystemResource.java
@@ -1,10 +1,12 @@
 package es.gusguma.template.microservice.eureka.server;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping(SystemResource.SYSTEM)
 public class SystemResource {
